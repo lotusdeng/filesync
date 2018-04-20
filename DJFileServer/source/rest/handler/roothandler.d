3dcdc23efb23d8c5be050697f36435ccee6d077b
lotusdeng2010@gmail.com <lotusdeng2010@gmail.com>
2018-04-20 17:29:26 +0800

    断点

--- DJFileServer/source/rest/handler/roothandler.d
+++
@@ -1,7 +0,0 @@
-module rest.handler.roothandler;
-import vibe.http.server;
-
-void handleGetRoot(HTTPServerRequest req, HTTPServerResponse res)
-{
-    res.writeBody("DJFileServer", "text/plain");
-}
